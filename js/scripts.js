a2c583cea35c8dcffc83bb43938c153290f0ebaf
Quadarius <qkmoore@crimson.ua.edu>
2014-11-15 21:40:40 -0600

    more new stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
- 
-var openText = function(){
+ var openText = function() {
 
-  var file = this.files[0];
+     var file = this.files[0];
+     var doc = [];
 
-  var reader = new FileReader();
-  reader.onload = function(progressEvent){
-    // Entire file
-    console.log(this.result);
-     // = this.result;
-    // By lines
-    var newPara = document.createElement("p");
-    var lines = this.result.split('\n');
-    for(var line = 0; line < lines.length; line++){
-      var newLineOfText = document.createTextNode(lines[line] + "\n\n");
-      document.getElementById("textention").appendChild(newLineOfText);
-      console.log(lines[line]);
-    }
+     var reader = new FileReader();
+     reader.onload = function(progressEvent) {
+         // Entire file
+         console.log(this.result);
+         // = this.result;
+         // By lines
+         var newPara = document.createElement("p");
+         var lines = this.result.split('\n');
+         for (var line = 0; line < lines.length; line++) {
+             var newLineOfText = document.createTextNode(lines[line] + "\n\n");
+             newPara.appendChild(newLineOfText);
+             doc.append(newPara);
+             console.log(lines[line]);
+         }
 
 
-  };
-  hideInputPanel();
-  reader.readAsText(file);
-};
-document.getElementById('file').onchange = openText;
-// document.getElementById('openfile').onclick = alert('click');
+     };
+     hideInputPanel();
+     reader.readAsText(file);
+ };
+ document.getElementById('file').onchange = openText;
+ // document.getElementById('openfile').onclick = alert('click');
 
-$(function () {
-  $('#openfile').click(open);
-})
+ $(function() {
+     $('#openfile').click(open);
+ })
 
-function hideInputPanel(){
-  $('inputpanel').hide()
-}
-function open(){
-  openText();
-  alert('open')
-}
+ function hideInputPanel() {
+     $('inputpanel').hide()
+ }
+
+ function open() {
+     openText();
+     alert('open')
+ }
