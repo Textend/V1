10623bdcea5ff06afae431adf3f96e50bb58c505
Quadarius <qkmoore@crimson.ua.edu>
2014-11-16 23:15:44 -0600

    Titles Set themselves

---
+++ js/setTitle.js
@@ -0,0 +1,20 @@
+function setTitle(titleNum){
+
+            switch(titleNum){
+                case 1:
+                    $("#bookTitle").text("Moby Dick");
+                    break;
+                case 2:
+                    $("#bookTitle").text("Fundamentals of Fundamentals");
+                    break;
+                case 3:
+                    $("#bookTitle").text("Books: A History");
+                    break;
+                case 4:
+                    $("#bookTitle").text("Designing the User Interface");
+                    break;
+                case 5:
+                    $("#bookTitle").text("Designing the User Face");
+                    break;
+            }
+        }
