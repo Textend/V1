75505c70ff00edae7d7f3e732ed0e3b1c382e8f4
Quadarius <qkmoore@crimson.ua.edu>
2014-11-17 01:03:47 -0600

    better titles

--- js/setTitle.js
+++ js/setTitle.js
@@ -2,19 +2,19 @@ function setTitle(titleNum){
 
             switch(titleNum){
                 case 1:
-                    $("#bookTitle").text("Moby Dick");
+                    $("*.bookTitle").text("Moby Dick");
                     break;
                 case 2:
-                    $("#bookTitle").text("Fundamentals of Fundamentals");
+                    $("*.bookTitle").text("Fundamentals of Fundamentals");
                     break;
                 case 3:
-                    $("#bookTitle").text("Books: A History");
+                    $("*.bookTitle").text("Books: A History");
                     break;
                 case 4:
-                    $("#bookTitle").text("Designing the User Interface");
+                    $("*.bookTitle").text("Designing the User Interface");
                     break;
                 case 5:
-                    $("#bookTitle").text("Designing the User Face");
+                    $("*.bookTitle").text("Designing the User Face");
                     break;
             }
         }
