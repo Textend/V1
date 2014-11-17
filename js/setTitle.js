37cd1db0089fab823d86bb6584288c655716be97
Quadarius <qkmoore@crimson.ua.edu>
2014-11-17 08:13:27 -0600

    Added change document prompt and authors

--- js/setTitle.js
+++ js/setTitle.js
@@ -3,18 +3,23 @@ function setTitle(titleNum){
             switch(titleNum){
                 case 1:
                     $("*.bookTitle").text("Moby Dick");
+                    $("*.bookAuthor").text("Herman Melville");
                     break;
                 case 2:
                     $("*.bookTitle").text("Fundamentals of Fundamentals");
+                    $("*.bookAuthor").text("Quadarius Moore");
                     break;
                 case 3:
                     $("*.bookTitle").text("Books: A History");
+                    $("*.bookAuthor").text("Brian Booker");
                     break;
                 case 4:
                     $("*.bookTitle").text("Designing the User Interface");
+                    $("*.bookAuthor").text("Ben Shneiderman");
                     break;
                 case 5:
                     $("*.bookTitle").text("Designing the User Face");
+                    $("*.bookAuthor").text("Nicholas Cage");
                     break;
             }
         }
