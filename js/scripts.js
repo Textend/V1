a07569b2e70aa327116a8f7f04eb4a5ae1828856
Steven Hohs <stevenhohs@gmail.com>
2014-12-02 00:06:42 -0600

    Slightly modified file loading
    Going to switch to another branch for a second

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
- var openText = function() {
+var openText = function() {
 
-     var file = this.files[0];
+    var file = this.files[0];
 
-     var reader = new FileReader();
-     reader.onload = function(progressEvent) {
-         // Entire file
-         console.log(this.result);
-         document.getElementById("openedfile").innerHTML = this.result;
+    var reader = new FileReader();
+    reader.onload = function(progressEvent) {
+        // Entire file
+        console.log(this.result);
+        // document.getElementById("openedfile").innerHTML = this.result;
 
-         var lines = this.result.split('\n');
+        var outHTML = "";
+        var lines = this.result.split('\n');
 
-         for (var line = 0; line < lines.length; line++) {
-             var newLineOfText = document.createTextNode(lines[line] + "\n\n");
-             console.log(lines[line]);
-         }
+        for (var line = 0; line < lines.length; line++) {
+            outHTML += "<p class=textention id=\"textline" + line + "\">" + lines[line] + "</p>";
+            // var newLineOfText = document.createTextNode(lines[line] + "\n\n");
+            // console.log(lines[line]);
+        }
 
+        document.getElementById("doc").innerHTML = outHTML;
+    };
+    reader.readAsText(file);
+};
 
-     };
-     reader.readAsText(file);
- };
-
- document.getElementById('file').onchange = openText;
- // document.getElementById('openfile').onclick = alert('click');
+document.getElementById('file').onchange = openText;
+// document.getElementById('openfile').onclick = alert('click');
